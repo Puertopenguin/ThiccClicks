3285720c4ae3d82bf87d80dde088ca0e3b02318c
Dre <puertopenguin@gmail.com>
2018-03-04 19:05:01 -0500

    This is the updated version thats giving me the GET/ - - ms -- message JAKE HELP

--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping-cart' });
+  Product.find(function(err, docs){
+      var productChunks = [];
+      var chunkSize = 2;
+      for (var i=0; i < docs.length; i+= chunkSize){
+          productChunks.push(docs.slice(i, i + chunkSize));
+      }
+      res.render('shop/index', { title: 'Shopping-cart', products: docs });
+  });
 });
 
 module.exports = router;
